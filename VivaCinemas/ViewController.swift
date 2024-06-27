b3fff862f9336886bfad94b068d2003213fe2b13
Andre Haas <andrehaas2005@gmail.com>
2024-06-27 11:57:32 -0300

    Initial Commit

---
+++ VivaCinemas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VivaCinemas
+//
+//  Created by Andre  Haas on 27/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
